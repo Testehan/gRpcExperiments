17faae62b9fbb09e74f8866dd07bd5fd40c5fd6c
DanTestehan <tdan89@yahoo.com>
2024-03-27 20:20:20 +0100

    server streaming example

--- gRPC-unary/src/main/java/com/testehan/unary/server/BankService.java
+++ gRPC-unary/src/main/java/com/testehan/unary/server/BankService.java
@@ -1,9 +1,8 @@
 package com.testehan.unary.server;
 
-import com.testehan.models.ex08.AccountBalance;
-import com.testehan.models.ex08.BalanceCheckRequest;
-import com.testehan.models.ex08.BankServiceGrpc;
+import com.testehan.models.ex08.*;
 import com.testehan.unary.server.repository.AccountRepository;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase{
@@ -20,6 +19,35 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase{
                 .build();
         // set the value to be returned
         responseObserver.onNext(accountBalance);
+
+        // mark operation as completed
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withdraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+        var accountNumber = request.getAccountNumber();
+        var amount = request.getAmount();   // for this example we assume that this number is a multiple of 10
+        System.out.println("Got a withdraw request for acc number : " + accountNumber);
+
+        var balance = AccountRepository.getBalance(accountNumber);
+        if (balance < amount) {
+            Status notEnoughtMoneyToWithdraw = Status.FAILED_PRECONDITION.withDescription("Not enought money to withdraw");
+            responseObserver.onError(notEnoughtMoneyToWithdraw.asRuntimeException());
+            return;
+        }
+
+        for (int i = 0; i< amount / 10; i++){   // because we send back in 10 dollar increments
+            var money = Money.newBuilder().setAmount(10).build();
+            responseObserver.onNext(money);
+            AccountRepository.deductAmount(accountNumber,10);
+            try {
+                System.out.println("Sending 10 money !");
+                Thread.sleep(5000);     // for testing purposes :)
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         // mark operation as completed
         responseObserver.onCompleted();
