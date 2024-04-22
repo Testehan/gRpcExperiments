4f8bac91be5e15eb848a983471b2a4afa6940211
DanTestehan <tdan89@yahoo.com>
2024-04-22 20:20:20 +0100

    main and submodules for grpc spring boot app

---
+++ gRPC-spring-boot/src/main/java/com/testehan/grpc/spring/GRpcSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.testehan.grpc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GRpcSpringBootApplication.class, args);
+	}
+
+}
