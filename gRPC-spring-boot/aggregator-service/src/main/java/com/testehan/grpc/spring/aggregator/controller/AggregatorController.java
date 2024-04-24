7bb70f207bcb97a69f4694ea8d69b98edf33ab76
DanTestehan <tdan89@yahoo.com>
2024-04-24 20:20:20 +0100

    grpc spring app - implemented aggregator service

---
+++ gRPC-spring-boot/aggregator-service/src/main/java/com/testehan/grpc/spring/aggregator/controller/AggregatorController.java
@@ -0,0 +1,27 @@
+package com.testehan.grpc.spring.aggregator.controller;
+
+import com.testehan.grpc.spring.aggregator.dto.RecommendedMovie;
+import com.testehan.grpc.spring.aggregator.dto.UserGenre;
+import com.testehan.grpc.spring.aggregator.service.UserMovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class AggregatorController {
+
+    @Autowired
+    private UserMovieService userMovieService;
+
+    @GetMapping("/user/{loginId}")
+    public List<RecommendedMovie> getMovies(@PathVariable String loginId){
+        return this.userMovieService.getUserMovieSuggestions(loginId);
+    }
+
+    @PutMapping("/user")
+    public void setUserGenre(@RequestBody UserGenre userGenre){
+        this.userMovieService.setUserGenre(userGenre);
+    }
+
+}
---
+++ gRPC-spring-boot/aggregator-service/src/main/java/com/testehan/grpc/spring/aggregator/service/UserMovieService.java
@@ -0,0 +1,51 @@
+package com.testehan.grpc.spring.aggregator.service;
+
+import com.testehan.grpc.spring.aggregator.dto.RecommendedMovie;
+import com.testehan.grpc.spring.aggregator.dto.UserGenre;
+import com.testehan.grpc.spring.common.Genre;
+import com.testehan.grpc.spring.movie.MovieSearchRequest;
+import com.testehan.grpc.spring.movie.MovieSearchResponse;
+import com.testehan.grpc.spring.movie.MovieServiceGrpc;
+import com.testehan.grpc.spring.user.UserGenreUpdateRequest;
+import com.testehan.grpc.spring.user.UserResponse;
+import com.testehan.grpc.spring.user.UserSearchRequest;
+import com.testehan.grpc.spring.user.UserServiceGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserMovieService {
+
+    @GrpcClient("user-service")
+    private UserServiceGrpc.UserServiceBlockingStub userStub;
+
+    @GrpcClient("movie-service")
+    private MovieServiceGrpc.MovieServiceBlockingStub movieStub;
+
+    public List<RecommendedMovie> getUserMovieSuggestions(String loginId){
+        UserSearchRequest userSearchRequest = UserSearchRequest.newBuilder().setLoginId(loginId).build();
+        UserResponse userResponse = this.userStub.getUserGenre(userSearchRequest);
+
+        MovieSearchRequest movieSearchRequest = MovieSearchRequest.newBuilder().setGenre(userResponse.getGenre()).build();
+        MovieSearchResponse movieSearchResponse = this.movieStub.getMovies(movieSearchRequest);
+
+        return movieSearchResponse.getMovieList()
+                .stream()
+                .map(movieDto -> new RecommendedMovie(movieDto.getTitle(), movieDto.getYear(), movieDto.getRating()))
+                .collect(Collectors.toList());
+    }
+
+    public void setUserGenre(UserGenre userGenre){
+        UserGenreUpdateRequest userGenreUpdateRequest = UserGenreUpdateRequest.newBuilder()
+                .setLoginId(userGenre.getLoginId())
+                .setGenre(Genre.valueOf(userGenre.getGenre().toUpperCase()))
+                .build();
+
+        UserResponse userResponse = this.userStub.updateUserGenre(userGenreUpdateRequest);
+    }
+
+
+}
